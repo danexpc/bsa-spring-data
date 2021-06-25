06f5f6e32a50849d405f847ab2367e5755741bab
Dan Smahliuk <55285854+danexpc@users.noreply.github.com>
2021-06-25 20:49:01 +0300

    Implement method which deletes users by experience and returns a number of deleted rows

--- src/main/java/com/bsa/springdata/user/UserRepository.java
+++ src/main/java/com/bsa/springdata/user/UserRepository.java
@@ -1,8 +1,12 @@
 package com.bsa.springdata.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
+
+    @Modifying
+    int deleteByExperience(int experience);
 }
