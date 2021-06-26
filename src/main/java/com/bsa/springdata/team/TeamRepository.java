ae200ecc3c99c837c7aad0393e953bc84baac7cd
Dan Smahliuk <55285854+danexpc@users.noreply.github.com>
2021-06-26 08:22:20 +0300

    Add method-query which update techonology whithin a team by technology_name and devs number

--- src/main/java/com/bsa/springdata/team/TeamRepository.java
+++ src/main/java/com/bsa/springdata/team/TeamRepository.java
@@ -1,8 +1,18 @@
 package com.bsa.springdata.team;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface TeamRepository extends JpaRepository<Team, UUID> {
+
+    @Query("select t from Team t where t.technology.name = :technologyName")
+    List<Team> findByTechnologyName(String technologyName);
+
+
+    void normalizeName(String name);
 }
+
--- src/main/java/com/bsa/springdata/team/TeamService.java
+++ src/main/java/com/bsa/springdata/team/TeamService.java
@@ -3,6 +3,8 @@ package com.bsa.springdata.team;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class TeamService {
     @Autowired
@@ -12,6 +14,15 @@ public class TeamService {
 
     public void updateTechnology(int devsNumber, String oldTechnologyName, String newTechnologyName) {
         // TODO: You can use several queries here. Try to keep it as simple as possible
+        List<Team> teams = teamRepository.findByTechnologyName(oldTechnologyName);
+        teams.stream()
+                .filter(team -> team.getUsers().size() < devsNumber)
+                .forEach(team -> {
+                    var technology = team.getTechnology();
+                    technology.setName(newTechnologyName);
+                    team.setTechnology(technology);
+                });
+        teamRepository.saveAll(teams);
     }
 
     public void normalizeName(String hipsters) {
