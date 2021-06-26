20226553603819a042d2a59a46d9297aa8c5c8d9
Dan Smahliuk <55285854+danexpc@users.noreply.github.com>
2021-06-26 16:00:58 +0300

    Add method-query which returns count of all projects with role

--- src/main/java/com/bsa/springdata/project/ProjectRepository.java
+++ src/main/java/com/bsa/springdata/project/ProjectRepository.java
@@ -1,8 +1,15 @@
 package com.bsa.springdata.project;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.UUID;
 
 public interface ProjectRepository extends JpaRepository<Project, UUID> {
+
+    @Query(value = "select count( distinct p) from projects p " +
+                    "join teams t on p.id = t.project_id " +
+                    "join users u on t.id = u.team_id " +
+                    "join user2role u2r on u.id = u2r.user_id ", nativeQuery = true)
+    int getCountWithRole(String role);
 }
