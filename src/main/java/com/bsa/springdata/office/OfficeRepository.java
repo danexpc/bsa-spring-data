6c2739cd4da6cc8982729f0c22e60ef3fe47684b
Dan Smahliuk <55285854+danexpc@users.noreply.github.com>
2021-06-26 10:45:43 +0300

    Add method-query which returns all offices in which teams are working with technology

--- src/main/java/com/bsa/springdata/office/OfficeRepository.java
+++ src/main/java/com/bsa/springdata/office/OfficeRepository.java
@@ -1,8 +1,17 @@
 package com.bsa.springdata.office;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface OfficeRepository extends JpaRepository<Office, UUID> {
+
+    @Query("select distinct o from Office o " +
+            "join o.users u " +
+            "join u.team t " +
+            "join t.technology techno " +
+            "where techno.name = :technology")
+    List<Office> getByTechnology(String technology);
 }
