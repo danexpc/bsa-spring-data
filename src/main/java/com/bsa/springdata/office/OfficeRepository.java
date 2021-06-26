4bda7f4a90a05452ad1bd4aabff62f186e74ef91
Dan Smahliuk <55285854+danexpc@users.noreply.github.com>
2021-06-26 15:50:07 +0300

    Add method-query which returns updates office if all users are working on project

--- src/main/java/com/bsa/springdata/office/OfficeRepository.java
+++ src/main/java/com/bsa/springdata/office/OfficeRepository.java
@@ -1,9 +1,11 @@
 package com.bsa.springdata.office;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface OfficeRepository extends JpaRepository<Office, UUID> {
@@ -14,4 +16,10 @@ public interface OfficeRepository extends JpaRepository<Office, UUID> {
             "join t.technology techno " +
             "where techno.name = :technology")
     List<Office> getByTechnology(String technology);
+
+    @Modifying
+    @Query("update Office o set o.address = :newAddress where size(o.users) <> 0 and o.address = :oldAddress")
+    void updateAddressIfAllUsersInProject(String oldAddress, String newAddress);
+
+    Optional<Office> getByAddress(String address);
 }
--- src/main/java/com/bsa/springdata/office/OfficeService.java
+++ src/main/java/com/bsa/springdata/office/OfficeService.java
@@ -3,6 +3,7 @@ package com.bsa.springdata.office;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -20,9 +21,13 @@ public class OfficeService {
                 .collect(Collectors.toList());
     }
 
+    @Transactional
     public Optional<OfficeDto> updateAddress(String oldAddress, String newAddress) {
         // TODO: Use single method to update address. In order to get the new office you can make extra query
         //  Hint: Every user is connected to one of the project. There cannot be any users without a project.
-        return null;
+        officeRepository.updateAddressIfAllUsersInProject(oldAddress, newAddress);
+        return officeRepository
+                .getByAddress(newAddress)
+                .map(OfficeDto::fromEntity);
     }
 }
