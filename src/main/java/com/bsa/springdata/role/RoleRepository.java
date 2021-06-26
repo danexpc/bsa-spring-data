9128170f2b9af09a41d3ccaac4f0cb4154679cbd
Dan Smahliuk <55285854+danexpc@users.noreply.github.com>
2021-06-26 10:22:25 +0300

    Add method-query which delete by code all roles which not matches any users

--- src/main/java/com/bsa/springdata/role/RoleRepository.java
+++ src/main/java/com/bsa/springdata/role/RoleRepository.java
@@ -2,11 +2,16 @@ package com.bsa.springdata.role;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.UUID;
 
 public interface RoleRepository extends JpaRepository<Role, UUID> {
 
     @Modifying
-    void deleteByCode(String code);
+    @Query(
+            value = "DELETE FROM roles r WHERE r.code = :code AND r.id NOT IN " +
+                    "(SELECT role_id FROM user2role)",
+            nativeQuery = true)
+    void deleteByCodeWhichNotMatchesAnyUsers(String code);
 }
--- src/main/java/com/bsa/springdata/role/RoleService.java
+++ src/main/java/com/bsa/springdata/role/RoleService.java
@@ -3,13 +3,16 @@ package com.bsa.springdata.role;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+
 @Service
 public class RoleService {
     @Autowired
     private RoleRepository roleRepository;
 
+    @Transactional
     public void deleteRole(String roleCode) {
         // TODO: Use a single query
-        roleRepository.deleteByCode(roleCode);
+        roleRepository.deleteByCodeWhichNotMatchesAnyUsers(roleCode);
     }
 }
